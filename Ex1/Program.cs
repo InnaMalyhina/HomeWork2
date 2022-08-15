f408d89dcf758ebd316b0650e4eaec267551000b
stopudof <stopudof@mail.ru>
2022-08-15 17:23:52 +0300

    Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Напишите программу,которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.*/
+
+Console.WriteLine ("Введите трёхзначное число: ");
+
+int treeNums = int.Parse (Console.ReadLine());
+string num = Convert.ToString (treeNums);
+
+Console.WriteLine ("Вторая цифра данного числа: " + num[1]);
