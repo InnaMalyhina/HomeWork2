bb091b19e4a8826bd0e2166a4b2b7515f9bc803d
stopudof <stopudof@mail.ru>
2022-08-15 17:25:57 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели и является ли этот день выходным.*/
+
+Console.WriteLine ("Введите цифру, которая обозначает день недели: ");
+int numDay = int.Parse (Console.ReadLine());
+if (numDay == 6 || numDay == 7)
+{
+    Console.WriteLine ("(Выходной) - да,является");
+}
+else if (numDay < 1 || numDay > 7)
+{
+    Console.WriteLine ("Такого дня не существует");
+}
+else
+{
+    Console.WriteLine ("(Будний день) - не является выходным");
+}
