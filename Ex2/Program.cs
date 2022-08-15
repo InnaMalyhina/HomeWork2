e923f235828bfa6f5e61fd3aa59706a6e8b1e0da
stopudof <stopudof@mail.ru>
2022-08-15 17:25:22 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа  или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine ("Введите число: ");
+int num = int.Parse (Console.ReadLine());
+string treeNum = Convert.ToString (num);
+
+if (treeNum.Length > 2)
+{
+    Console.WriteLine ("Третья цифра: "+ treeNum [2]);
+}
+else
+{
+    Console.WriteLine ("Третьей цифры нет!");
+}
